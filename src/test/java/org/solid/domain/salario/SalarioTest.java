7abfc120016b10c6cdf3025c7f8a6456ebc65b55
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2023-10-28 19:34:17 -0500

    actualizacion test salario

--- src/test/java/org/solid/domain/salario/SalarioTest.java
+++ src/test/java/org/solid/domain/salario/SalarioTest.java
@@ -2,21 +2,27 @@ package org.solid.domain.salario;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.solid.domain.asistencia.HorasExtra;
 
+@ExtendWith(MockitoExtension.class)
 class SalarioTest {
-
-    @Mock
-    private Salario salario;
+    private SalarioHoraExtra salarioHoraExtra;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        HorasExtra mockHorasExtra = mock(HorasExtra.class);
+        salarioHoraExtra = new SalarioHoraExtra(1000.0, mockHorasExtra);
     }
+    @Mock
+    private Salario salario;
 
     @Test
     void testSalarioBaseMensualGetterAndSetter() {
@@ -44,5 +50,65 @@ class SalarioTest {
         double resultado = salario.calcularSalario();
         assertEquals(1500.0, resultado);
     }
+
+    @Test
+    void testCalcularSalarioQuincenal() {
+        SalarioQuincenal salarioQuincenal = new SalarioQuincenal(6000.0);
+        double salarioQuincenalCalculado = salarioQuincenal.calcularSalario();
+        assertEquals(3000.0, salarioQuincenalCalculado);
+    }
+
+    @Test
+    void testCalcularSalarioPorSemana() {
+        SalarioPorSemana salarioPorSemana = new SalarioPorSemana(4000.0);
+        double salarioPorSemanaCalculado = salarioPorSemana.calcularSalario();
+        assertEquals(1000.0, salarioPorSemanaCalculado);
+    }
+
+    @Test
+    void testCalcularSalarioPorHora() {
+        SalarioPorHora salarioPorHora = new SalarioPorHora(3000.0);
+        double salarioPorHoraCalculado = salarioPorHora.calcularSalario();
+        assertEquals(12.5, salarioPorHoraCalculado, 0.01);
+    }
+
+    @Test
+    void testCalcularSalarioPorDia() {
+        SalarioPorDia salarioPorDia = new SalarioPorDia(3000.0);
+        double salarioPorDiaCalculado = salarioPorDia.calcularSalario();
+        assertEquals(100.0, salarioPorDiaCalculado, 0.01);
+    }
+
+    @Test
+    void testCalcularSalarioMensual() {
+        SalarioMensual salarioMensual = new SalarioMensual(5000.0);
+        double salarioMensualCalculado = salarioMensual.calcularSalario();
+        assertEquals(5000.0, salarioMensualCalculado);
+    }
+
+    @Test
+    void testCalcularSalarioHoraExtra() {
+        HorasExtra mockHorasExtra = Mockito.mock(HorasExtra.class);
+        when(mockHorasExtra.getHorasDiurnas()).thenReturn(10);
+        when(mockHorasExtra.getHorasNocturnas()).thenReturn(5);
+        SalarioHoraExtra salarioHoraExtra = new SalarioHoraExtra(200.0, mockHorasExtra);
+        double salario = salarioHoraExtra.calcularSalario();
+        assertEquals(13.541666666666668, salario);
+    }
+
+    @Test
+    void testSalarioPorHorasDiurnasGetterAndSetter() {
+        double expectedSalarioPorHorasDiurnas = 500.0;
+        salarioHoraExtra.setSalarioPorHorasDiurnas(expectedSalarioPorHorasDiurnas);
+        assertEquals(expectedSalarioPorHorasDiurnas, salarioHoraExtra.getSalarioPorHorasDiurnas());
+    }
+
+    @Test
+    void testSalarioPorHorasNocturnasGetterAndSetter() {
+        double expectedSalarioPorHorasNocturnas = 250.0;
+        salarioHoraExtra.setSalarioPorHorasNocturnas(expectedSalarioPorHorasNocturnas);
+        assertEquals(expectedSalarioPorHorasNocturnas, salarioHoraExtra.getSalarioPorHorasNocturnas());
+    }
+
 }
 
